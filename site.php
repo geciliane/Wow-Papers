9b4eef7d361bd807a864e24394307cfd649f12e9
Geciliane Araujo de Lima <geciliane.lima@docasdoceara.com.br>
2019-04-26 09:02:25 -0300

    testando

---
+++ site.php
@@ -0,0 +1,10 @@
+<?php
+require_once __DIR__ . 'lib/Facebook/autoload.php'; // change path as needed
+
+$fb = new \Facebook\Facebook([
+  'app_id' => '{app-id}',
+  'app_secret' => '{app-secret}',
+  'default_graph_version' => 'v2.10',
+  //'default_access_token' => '{access-token}', // optional
+]);
+?>
